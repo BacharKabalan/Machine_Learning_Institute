49ec06d5adef64e0584406dd2c7b5d10c31b6457
BacharKabalan <basharkabalan@gmail.com>
2023-11-22 12:38:17 +0000

    modifying the eval.py function to successfully load trained model

--- week5_fine_tuning_LLAMA2/eval.py
+++ week5_fine_tuning_LLAMA2/eval.py
@@ -5,16 +5,22 @@ import peft
 import time
 #%%
 tokenizer = t.AutoTokenizer.from_pretrained("NousResearch/Llama-2-7b-hf")
-model = t.AutoModelForCausalLM.from_pretrained("NousResearch/Llama-2-7b-hf", load_in_8bit=True, torch_dtype=torch.float16)
+base_model = t.AutoModelForCausalLM.from_pretrained("NousResearch/Llama-2-7b-hf", load_in_8bit=True, torch_dtype=torch.float16)
 tokenizer.pad_token_id = 0
 #%%
-config = peft.LoraConfig(r=8, lora_alpha=16, target_modules=["q_proj", "v_proj"], lora_dropout=0.005, bias="none", task_type="CAUSAL_LM")
-model = peft.get_peft_model(model, config)
-# peft.set_peft_model_state_dict(model, torch.load("./output/checkpoint-600/adapter_model.bin"))
+config = peft.PeftConfig.from_pretrained("./output/checkpoint-4900")
+model = peft.PeftModel.from_pretrained(base_model, "./output/checkpoint-4900")
+# peft.set_peft_model_state_dict(model,"./output/checkpoint-4900/adapter_config.json")
 #%%
-TEMPLATE = "Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.\n\n### Instruction:\n{instruction}\n\n### Response:\n"
-INSTRUCTION = "Python how to insert something at the beginning of a list?"
-prompt = TEMPLATE.format(instruction=INSTRUCTION)
+
+########### ADD Template in a file
+TEMPLATE_YES_INPUT = "Below is a question that describes a task paired with further context. Write a response that appropriately completes the request.\n\n### question:\n{question}\n\n### context:\n{context}\n\n### Response:\n"
+QUESTION = "How many heads of the departments are older than 56 ?"
+CONTEXT = "CREATE TABLE head (age INTEGER)"
+prompt = TEMPLATE_YES_INPUT.format(question=QUESTION, context=CONTEXT)
 #%%
+print(prompt)
 pipe = t.pipeline(task="text-generation", model=model, tokenizer=tokenizer, max_length=500)
+print(11111111111)
+print(pipe(prompt)[0]['generated_text'])
 print("pipe(prompt)", pipe(prompt))
