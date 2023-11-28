49aa2a94100ef54c99038e16f8f5952a592eee07
BacharKabalan <basharkabalan@gmail.com>
2023-11-28 17:01:10 +0000

    updaing setup_script.sh to include moving .gitignore from local to remote on setup

--- setup/setup_script.sh
+++ setup/setup_script.sh
@@ -11,3 +11,4 @@ PORT="$4"
 scp -i "$PRIVATE_KEY" -P "$PORT" /home/bash1989/.ssh/id_ed25519.pub "$USERNAME"@"$HOST":~/.ssh/
 scp -i "$PRIVATE_KEY" -P "$PORT" /home/bash1989/.ssh/id_ed25519 "$USERNAME"@"$HOST":~/.ssh/
 scp -i "$PRIVATE_KEY" -P "$PORT" ~/founders_and_coders/founders_and_coders/setup/server_setup_script.sh "$USERNAME"@"$HOST":/workspace/
+ scp -i "$PRIVATE_KEY" -P "$PORT" ~/founders_and_coders/founders_and_coders/.gitignore "$USERNAME"@"$HOST":/workspace/Machine_Learning_Institute/
