af4e75152c3e0f2bfc0092d815768b1be3d1c3e7
BacharKabalan <basharkabalan@gmail.com>
2023-12-12 17:38:38 +0000

    succesful training of dreambooth. Next step is to work on enhancing the quality of the output. I have tested overfitting on one image. NExt step is to overfit on multiple images.

---
+++ week8_fine_tune_stable_diffusion/dreambooth/training.py
@@ -0,0 +1,21 @@
+import os
+
+accelerate config default --mixed_precision fp16
+
+export MODEL_NAME = 'CompVis/stable-diffusion-v1-4'
+export INSTANCE_DIR = './bash_images'
+export OUTPUT_DIR = './dreambooth/sd_aiconos-model-v1-2_400'
+
+
+accelerate launch ./diffusers/examples/dreambooth/train_dreambooth.py \
+  --pretrained_model_name_or_path=$MODEL_NAME  \
+  --instance_data_dir=$INSTANCE_DIR \
+  --output_dir=$OUTPUT_DIR \
+  --instance_prompt="a photo of brk1" \
+  --resolution=512 \
+  --train_batch_size=1 \
+  --gradient_accumulation_steps=1 \
+  --learning_rate=5e-6 \
+  --lr_scheduler="constant" \
+  --lr_warmup_steps=0 \
+  --max_train_steps=600
